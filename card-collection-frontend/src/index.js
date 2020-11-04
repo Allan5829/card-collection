0f30a7fe6078a5f47aac35857bc935113c9b94bb
Allan5829 <allan.farca@gmail.com>
2020-11-03 22:51:05 -0800

    adds html to index.html. Removes fetch test from controller, routes, and index.js.

--- card-collection-frontend/src/index.js
+++ card-collection-frontend/src/index.js
@@ -1,10 +1,3 @@
-console.log("testing...")
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
-  const BACKEND_URL = 'localhost:3000';
-  fetch(`${BACKEND_URL}/test`)
-    .then(response => response.json())
-    .then(parsedResponse => console.log(parsedResponse));
+  console.log("DOMContentLoaded")
 });
