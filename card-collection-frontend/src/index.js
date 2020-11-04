6015bd7177f301b0983cf866e512e40e4e95d937
Allan5829 <allan.farca@gmail.com>
2020-11-03 23:25:11 -0800

    creates temporary javascript to test implementation.

--- card-collection-frontend/src/index.js
+++ card-collection-frontend/src/index.js
@@ -1,3 +1,23 @@
+const collectionContainer = document.getElementById("collection-container");
+
+let notListedTable = document.getElementById("not-listed");
+let testButton = document.getElementById("test-button")
+let test = `
+            <tr>
+              <th> Not Listed </th>
+            </tr>
+
+            <tr> 
+              <td> - indvidual card </td>
+            </tr>
+            `
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded")
+  testButton.addEventListener("click", testFunction)
 });
+
+function testFunction() {
+  console.log("do I see this?")
+  notListedTable.innerHTML += test
+}
