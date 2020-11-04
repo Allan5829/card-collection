cad9d47d39cbc2ca10dd067fdb486674a387e032
Allan5829 <allan.farca@gmail.com>
2020-11-03 23:52:55 -0800

    creates class method to create table html using fake data.

--- card-collection-frontend/src/index.js
+++ card-collection-frontend/src/index.js
@@ -1,6 +1,6 @@
 const collectionContainer = document.getElementById("collection-container");
 
-let notListedTable = document.getElementById("not-listed");
+let notListedTable = document.getElementById("not-listed"); //delete
 let testButton = document.getElementById("test-button")
 let test = `
             <tr>
@@ -15,9 +15,20 @@ let test = `
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded")
   testButton.addEventListener("click", testFunction)
+  Generate.createCollectionTables()
 });
 
-function testFunction() {
+function testFunction() { //delete
   console.log("do I see this?")
   notListedTable.innerHTML += test
+}
+
+class Generate {
+
+  static createCollectionTables() {
+    // would fetch collection names
+    let collNames = ["Not Listed", "Vivid Voltage", "Darkness Ablaze"]
+    collNames.forEach(n => collectionContainer.innerHTML += `<table id="${n}"> </table>`)
+  }
+  
 }
