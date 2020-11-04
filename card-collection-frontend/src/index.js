ddc1ee4bdaa5f2ed9e9bae1ecc395d30704d5544
Allan5829 <allan.farca@gmail.com>
2020-11-03 22:32:50 -0800

    sets up files to allow frontend and backend communication. Includes a test fetch request.

--- card-collection-frontend/src/index.js
+++ card-collection-frontend/src/index.js
@@ -1,1 +1,10 @@
 console.log("testing...")
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const BACKEND_URL = 'localhost:3000';
+  fetch(`${BACKEND_URL}/test`)
+    .then(response => response.json())
+    .then(parsedResponse => console.log(parsedResponse));
+});
