5290dbecda3bc7723248f20801c9aef6f34aad24
Allan5829 <allan.farca@gmail.com>
2020-11-04 01:10:03 -0800

    creates class method to add a card to it's collection with no communication to backend as of now.

--- card-collection-frontend/src/index.js
+++ card-collection-frontend/src/index.js
@@ -20,20 +20,35 @@ class Generate {
   static createCollectionTables() {
     // would fetch collection names
     let collNames = ["Not Listed", "Vivid Voltage", "Darkness Ablaze"]
-    collNames.forEach(n => collectionContainer.innerHTML += `<table id="${n}"> </table>`)
+    collNames.forEach(function(n){
+      collectionContainer.innerHTML += `<table id="${n}"> </table>`
+      Generate.addHeaderToTable(n);
+    })
   }
 
   static addCardFormButton() {
     newCardForm.addEventListener("submit", function(event){
       event.preventDefault();
       console.log(document.getElementById("collection").value);
-      Generate.addHeaderToTable(document.getElementById("collection").value);
+
+      //bottom code will be relocated
+      let card = document.getElementById("card-name") //if card equals the value not the element 
+                                  //then I can't change the value of the card to an empty string
+      console.log(card.value);
+      Generate.addCardToTable(card.value);
+      card.value = ""
+      //debugger
     })
   }
 
-  static addHeaderToTable(value) { //temporary because will only occur if collection has no cards
+  static addHeaderToTable(value) {
     let table = document.getElementById(`${value}`);
     table.innerHTML += `<tr> <th> ${value} </th> </tr>` 
+  }
+
+  static addCardToTable(card) { //to be relocated
+    let table = document.getElementById("collection").value;
+    document.getElementById(table).innerHTML += `<tr> <td> ${card} </td> </tr>`
   }
 
 }
