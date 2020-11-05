c7d4307db7290797e310901ff55e679a1be97f8c
Allan5829 <allan.farca@gmail.com>
2020-11-05 04:15:22 -0800

    adds functionality for form drop down menu to generate new select options dynamically. Creates const for a html element used several times.

--- card-collection-frontend/src/index.js
+++ card-collection-frontend/src/index.js
@@ -1,4 +1,5 @@
 const collectionContainer = document.getElementById("collection-container");
+const collectionOptions = document.getElementById("form-collection-options")
 const newCardForm = document.getElementById("new-card-form"); 
 
 const BACKEND_URL = 'http://localhost:3000';
@@ -19,6 +20,7 @@ class Generate {
     let collNames = array.map(x => x.name)
     collNames.forEach(function(n){
       collectionContainer.innerHTML += `<table id="${n}"> </table>`
+      collectionOptions.innerHTML += `<option value="${n}">${n}</option>`
       Generate.addHeaderToTable(n);
     })
     Generate.addExistingCards(array);
@@ -39,7 +41,7 @@ class Generate {
   static addCardFormButton() {
     newCardForm.addEventListener("submit", function(event){
       event.preventDefault();
-      console.log(document.getElementById("collection").value);
+      console.log(collectionOptions.value);
 
       //bottom code will be relocated
       let card = document.getElementById("card-name") //if card equals the value not the element 
@@ -52,7 +54,7 @@ class Generate {
   }
 
   static addCardToTable(card) { //to be relocated
-    let table = document.getElementById("collection").value;
+    let table = collectionOptions.value;
     document.getElementById(table).innerHTML += `<tr id="card-row-${card}"> <td> ${card} </td> </tr>`
   }
 
