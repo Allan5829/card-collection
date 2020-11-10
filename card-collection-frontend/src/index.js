1eabcec5f678edf9b2cb11b7e8d56dcea6dd91a0
Allan5829 <allan.farca@gmail.com>
2020-11-10 03:38:29 -0800

    reorganized frontend code from two classes into three, adds a comment, and makes small edits to code.

--- card-collection-frontend/src/index.js
+++ card-collection-frontend/src/index.js
@@ -7,35 +7,57 @@ const BACKEND_URL = 'http://localhost:3000';
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded") //delete later
 
-  Fetch.fetchCollectionIndex();
+  Collection.fetchCollectionIndex();
   Generate.addCardFormButton();
 
 });
 
 //use to delete cards ->  document.getElementById(`card-row-${card.name}`).parentElement.remove()
 
-class Generate {
+class Collection {
+
+  static fetchCollectionIndex() {
+    fetch(`${BACKEND_URL}/collections`)
+      .then(response => response.json())
+      .then(collections => Collection.createCollectionTables(collections))
+  }
 
   static createCollectionTables(array) {
     let collNames = array.map(x => x.name)
-    collNames.forEach(function(n){
-      collectionContainer.innerHTML += `<table id="${n}"> </table>`
-      collectionOptions.innerHTML += `<option value="${n}">${n}</option>`
-      Generate.addHeaderToTable(n);
+    collNames.forEach(x => {
+      collectionContainer.innerHTML += `<table id="${x}"> </table>`
+      collectionOptions.innerHTML += `<option value="${x}">${x}</option>`
+      Generate.addHeaderToTable(x);
     })
-    Generate.addExistingCards(array);
+    Card.addExistingCards(array);
   }
+
+}
+
+class Card {
+
+  static addExistingCards(array) {
+    // for each collection, add some html with unique information for each card in a collection
+    array.forEach(function(collection) {
+      let set = document.getElementById(collection.name)
+      collection.cards.forEach(card => { 
+        set.innerHTML += `<tr id="card-row-${card.name}"> <td> ${card.name} </td> </tr>`
+      })
+    })
+  }
+
+  static addCardToTable(card) { //to be relocated
+    let table = collectionOptions.value;
+    document.getElementById(table).innerHTML += `<tr id="card-row-${card}"> <td> ${card} </td> </tr>`
+  }
+
+}
+
+class Generate {
 
   static addHeaderToTable(value) {
     let table = document.getElementById(`${value}`);
     table.innerHTML += `<tr id="coll-row-${value}"> <th> ${value} </th> </tr>` 
-  }
-
-  static addExistingCards(array) {
-    array.forEach(function(collection) {
-      let set = document.getElementById(collection.name)
-      collection.cards.forEach(card => set.innerHTML += `<tr id="card-row-${card.name}"> <td> ${card.name} </td> </tr>`)
-    })
   }
 
   static addCardFormButton() {
@@ -47,25 +69,10 @@ class Generate {
       let card = document.getElementById("card-name") //if card equals the value not the element 
                                   //then I can't change the value of the card to an empty string
       console.log(card.value);
-      Generate.addCardToTable(card.value);
+      Card.addCardToTable(card.value);
       card.value = ""
       //debugger
     })
-  }
-
-  static addCardToTable(card) { //to be relocated
-    let table = collectionOptions.value;
-    document.getElementById(table).innerHTML += `<tr id="card-row-${card}"> <td> ${card} </td> </tr>`
-  }
-
-}
-
-class Fetch {
-
-  static fetchCollectionIndex() {
-    fetch(`${BACKEND_URL}/collections`)
-      .then(response => response.json())
-      .then(collections => Generate.createCollectionTables(collections))
   }
 
 }
