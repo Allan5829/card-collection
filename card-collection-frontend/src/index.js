16bc794cc684b70dd951990a57f7eaeb568e6196
Allan5829 <allan.farca@gmail.com>
2020-11-10 04:48:01 -0800

    removes unneeded code, reorganizes exisiting code, and makes small edits.

--- card-collection-frontend/src/index.js
+++ card-collection-frontend/src/index.js
@@ -5,11 +5,8 @@ const newCardForm = document.getElementById("new-card-form");
 const BACKEND_URL = 'http://localhost:3000';
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOMContentLoaded") //delete later
-
   Collection.fetchCollectionIndex();
   Generate.addCardFormButton();
-
 });
 
 //use to delete cards ->  document.getElementById(`card-row-${card.name}`).parentElement.remove()
@@ -38,20 +35,12 @@ class Card {
 
   static addExistingCards(array) {
     // for each collection, add some html with unique information for each card in a collection
-    array.forEach(function(collection) {
+    array.forEach( collection => {
       let set = document.getElementById(collection.name)
       collection.cards.forEach(card => { 
         set.innerHTML += `<tr id="card-row-${card.name}"> <td> ${card.name} </td> </tr>`
       })
     })
-  }
-
-  static addCardToTable(card) {
-    //debugger
-    let table = document.getElementById(`${collectionOptions.value}`);
-    table.innerHTML += `<tr id="card-row-${card.name}"> <td> ${card.name} </td> </tr>`
-    let cardInput = document.getElementById("card-name")
-    cardInput.value = ""
   }
 
   static fetchCardNew() {
@@ -69,11 +58,16 @@ class Card {
       body: JSON.stringify(newCard)
     };
 
-    console.log(newCard)
     fetch(`${BACKEND_URL}/cards`, configObj)
       .then(response => response.json())
       .then(card => Card.addCardToTable(card))
-      //.then(card => console.log(card))
+  }
+
+  static addCardToTable(card) {
+    let table = document.getElementById(`${collectionOptions.value}`);
+    table.innerHTML += `<tr id="card-row-${card.name}"> <td> ${card.name} </td> </tr>`
+    let cardInput = document.getElementById("card-name")
+    cardInput.value = ""
   }
 
 }
@@ -86,17 +80,8 @@ class Generate {
   }
 
   static addCardFormButton() {
-    newCardForm.addEventListener("submit", function(event){
-      event.preventDefault();
-      console.log(collectionOptions.value);
-
-      //bottom code will be relocated
-      //let card = document.getElementById("card-name") //if card equals the value not the element 
-                                  //then I can't change the value of the card to an empty string
-      //console.log(card.value);
-      //Card.addCardToTable(card.value);
-      //card.value = ""
-      //debugger
+    newCardForm.addEventListener("submit", e => {
+      e.preventDefault();
       Card.fetchCardNew();
     })
   }
